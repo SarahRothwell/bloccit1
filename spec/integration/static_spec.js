19709f4a6a321d28b67bcf9bbcbb9d374376a3c9
Sarah Rothwell <sarahhrothwell@gmail.com>
2019-04-25 08:33:55 -0400

    final commit

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,6 +1,6 @@
 const request = require("request");
 const server = require("../../src/server");
-const base = "http://localhost:3000";
+const base = "http://localhost:3000/";
 
 describe("routes : static", () => {
 
@@ -26,7 +26,7 @@ describe("routes : static", () => {
 
   describe("GET /marco", () => {
     it("should return a status code 200 with a body response of polo", (done) => {
-      request.get('/marco',(err, res, body) => {
+      request.get(`${base}marco`,(err, res, body) => {
         console.log(res);
         expect(res.statusCode).toBe(200);
         expect(body).toContain("polo");
